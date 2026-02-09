0a5514f40843db9b2e61a325e974edfd4c462c6f
quim49 <jqmcarmona@gmail.com>
2026-02-09 20:03:36 +0100

    Semana 1: Teoria + recursos de funciones

---
+++ Semana_01/Teoria/#Fundamental Data Types.py
@@ -0,0 +1,20 @@
+#Fundamental Data Types
+# int and float
+print(type(2+4))
+print(2+4)
+print(type(2-4))
+print(2-4)
+print(type(2*4))
+print(2*4)
+print(type(2/4))
+print(2/4)
+
+print(type(10.56))
+print(2 ** 2)
+#Esto muestra que el operador // aproxima hacia abajo el resultado de una división entre dos números al número entero (o completo) más cercano.
+print(2//4)
+#Módulo, muestra el resto de la división
+print(5 % 4)
+
+#Siempre se necesitará más memoria para almacenar un float que un entero
+#Python automáticamente asigna el tipo en función del número que está trabajando
---
+++ Semana_01/Teoria/#Operator precedence.py
@@ -0,0 +1,2 @@
+#Operator precedence
+print(20 + 3 * 4)
